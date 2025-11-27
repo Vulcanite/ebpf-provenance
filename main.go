bf8e114bb5fe5a9a9ef9d29be78f218821be47f4
Vulcanite <amishtp147@outlook.com>
2025-11-28 03:27:55 +0800

    Timezone updated to UTC

--- main.go
+++ main.go
@@ -287,9 +287,10 @@ func main() {
 		}
 
 		raw := *(*bpfSoEvent)(unsafe.Pointer(&record.RawSample[0]))
+		ts := time.Unix(0, int64(raw.Timestamp)).UTC()
 		evt := AuditEvent{
 			TimestampNs: int64(raw.Timestamp),
-			Datetime:    time.Now().Format(time.RFC3339),
+			Datetime:    ts.Format(time.RFC3339Nano),
 			Pid:         raw.Pid,
 			Ppid:        raw.Ppid,
 			Uid:         raw.Uid,
